ad480e4022d775a1fef33290f5050b3bb532956b
MACIriarte <m.angelcaceres88@gmail.com>
2023-01-20 18:12:02 +0100

    main updated

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-from dictionaries import *
+import random
 import pymysql
+import pymysql.cursors
+from dictionaries import *
 
 connection = pymysql.connect(
-    host='localhost',
-    user='root',
-    password='12345678',
+    host='13.69.10.32',
+    user='alumno',
+    password='12345678Abc',
     db='db_game',
     charset='utf8mb4',
     cursorclass=pymysql.cursors.DictCursor
 )
 
-mainprogram()
